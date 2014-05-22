f7fd9e24561fc345109787abf678591c9d2b6900
Andrew Or <andrewor14@gmail.com>
2014-05-21 19:38:00 -0700

    Make the number of partitions configurable

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,6 +6,7 @@ import org.apache.spark.rdd.JdbcRDD
 
 /**
  * Main application class.
+ * Usage: Main [numPartitions]
  */
 object Main {
   def main(args: Array[String]) {
@@ -16,6 +17,7 @@ object Main {
     val password = sys.env("HANA_PASSWORD")
     val query = sys.env("HANA_QUERY")
     val url = s"jdbc:sap://$ip:30015/?autocommit=false"
+    val numPartitions = if (args.length > 0) args(0).toInt else 20
 
     def connectToHana(): Connection = {
       DriverManager.getConnection(url, user, password)
@@ -25,7 +27,7 @@ object Main {
 
     // Connect to Hana through JDBC
     Class.forName("com.sap.db.jdbc.Driver")
-    val rdd = new JdbcRDD(sc, connectToHana, query, 1, 1, 1, processResult)
+    val rdd = new JdbcRDD(sc, connectToHana, query, 1, 1, numPartitions, processResult)
     rdd.collect()
 
     println("Done.")
