f7b88727716c4c7452d1bfc46f0a55054bf397ad
Andrew Or <andrew@databricks.com>
2015-06-19 14:12:35 -0700

    Regression test for SPARK-8489

--- simple.sbt
+++ simple.sbt
@@ -6,6 +6,8 @@ scalaVersion := "2.10.4"
 
 resolvers += "Akka Repository" at "http://repo.akka.io/releases/"
 
-resolvers += "Spark 1.0 RC" at "https://repository.apache.org/content/repositories/orgapachespark-1018"
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.4.0"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "1.0.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "1.4.0"
+
+libraryDependencies += "org.apache.spark" %% "spark-hive" % "1.4.0"
--- src/main/scala/MyCoolClass.scala
+++ src/main/scala/MyCoolClass.scala
@@ -15,21 +15,6 @@
  * limitations under the License.
  */
 
-import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.hive.HiveContext
+/** Dummy class used in regression test for SPARK-8498. */
+case class MyCoolClass(past: String, present: String, future: String)
 
-/**
- * Entry point in test application for SPARK-8498.
- */
-object Main {
-  def main(args: Array[String]) {
-    println("Running regression test for SPARK-8498.")
-    val sc = new SparkContext("local", "testing")
-    val hc = new HiveContext(sc)
-    // This line should not throw scala.reflect.internal.MissingRequirementError.
-    // See SPARK-8470 for more detail.
-    val df = hc.createDataFrame(Seq(MyCoolClass("1", "2", "3")))
-    df.collect()
-    println("Regression test for SPARK-8498 success!")
-  }
-}
